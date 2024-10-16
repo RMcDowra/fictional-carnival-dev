7a71dc080cffa3fd83a7efcaa01948e33435aad0
Reed McDowra <xenoverseblack@reeds-mbp.main.local.una.edu>
2024-10-16 14:22:52 -0500

    change

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const mongoURI = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
